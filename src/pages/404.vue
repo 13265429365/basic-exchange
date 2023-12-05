e9fd67d042306120b0a0611cb35335c6491af6cb
SkyBlue <2576755215@qq.com>
2023-12-05 20:56:20 +0800

    开始接口
    开始接口

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -8,56 +8,58 @@
 </template>
 
 <script lang="ts">
-  import lang from 'src/components/lang/lang.vue';
-  import { defineComponent } from 'vue';
-  import { useRouter } from 'vue-router';
-  // 因为自动加载路由问题。 第一次会获取不到页面跳转到404页面，未修复
-  export default defineComponent({
-    components: {
-      lang,
-    },
-    name: 'errorView',
-    setup() {
-      const router = useRouter();
-      return {
-        newValue(newValue: void) {
-          console.log(newValue)
-        },
-        toLogin() {
-          router.push('Login')
-        },
-      }
+import lang from 'src/components/lang/lang.vue';
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+// 因为自动加载路由问题。 第一次会获取不到页面跳转到404页面，未修复
+export default defineComponent({
+  components: {
+    lang,
+  },
+  name: 'errorView',
+  setup() {
+    const router = useRouter();
+    return {
+      newValue(newValue: void) {
+        console.log(newValue)
+      },
+      toLogin() {
+        router.push('Login')
+      },
     }
-  });
+  }
+});
 </script>
 <style lang="scss" scoped>
-  .sub {
-    width: 100%;
-    height: 44px;
-    background: $default;
-    border-radius: 29px;
-    cursor: pointer;
-    color: #fff;
-    font-size: 15px;
-  }
-  .login {
-    color: $default;
-    cursor: pointer;
-  }
+.sub {
+  width: 100%;
+  height: 44px;
+  background: $default;
+  border-radius: 29px;
+  cursor: pointer;
+  color: #fff;
+  font-size: 15px;
+}
 
-  .logo {
-    width: 70px;
-    height: 70px;
-    margin-top: 30px;
-    margin-bottom: 20px;
-  }
+.login {
+  color: $default;
+  cursor: pointer;
+}
 
-  .iconLogo {
-    width: 24px;
-    height: 24px;
-  }
-  .countryLogo {
-    width: 24px;
-    height: 16px;
-  }
+.logo {
+  width: 70px;
+  height: 70px;
+  margin-top: 30px;
+  margin-bottom: 20px;
+}
+
+.iconLogo {
+  width: 24px;
+  height: 24px;
+}
+
+.countryLogo {
+  width: 24px;
+  height: 16px;
+}
 </style>
