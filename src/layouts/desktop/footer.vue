5f369ceffad8228fd20baadf91026a83f97ad9e8
饶兆锋 <245532242@qq.com>
2023-12-15 09:36:07 +0800

    footer 1200px

--- src/layouts/desktop/footer.vue
+++ src/layouts/desktop/footer.vue
@@ -1,39 +1,41 @@
 <template>
-  <div style="background: #021736;padding: 34px 10%;" class="text-white">
-    <div class="row">
-      <div class="col-9">
-        <div class="row justify-center">
-          <div v-for="(article, articleIndex) in articleList" :key="articleIndex" class="col-3">
-            <div class="column">
-              <div class="text-body1 text-bold">{{ article.name }}</div>
-              <div v-for="(children, childrenIndex) in article.children" :key="childrenIndex" style="width: 100%;">
-                <div class="ellipsis text-grey-4 text-body2 q-mt-sm cursor-pointer" @click="routerTo(children.route)">{{
-                  children.name }}</div>
+  <div style="background: #021736;padding: 36px" class="text-white column items-center">
+    <div style="width: 1200px;">
+      <div class="row">
+        <div class="col-9">
+          <div class="row justify-center">
+            <div v-for="(article, articleIndex) in articleList" :key="articleIndex" class="col-3">
+              <div class="column">
+                <div class="text-body1 text-bold">{{ article.name }}</div>
+                <div v-for="(children, childrenIndex) in article.children" :key="childrenIndex" style="width: 100%;">
+                  <div class="ellipsis text-grey-4 text-body2 q-mt-sm cursor-pointer" @click="routerTo(children.route)">{{
+                    children.name }}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-1"></div>
-      <div class="col-2">
-        <div class="text-body1 text-bold">社区</div>
-        <div class="row items-center q-gutter-sm">
-          <div v-for="(community, communityIndex) in communityList" :key="communityIndex">
-            <q-icon :name="community.icon" size="md" class="cursor-pointer" @click="routerTo(community.route)"></q-icon>
+        <div class="col-1"></div>
+        <div class="col-2">
+          <div class="text-body1 text-bold">社区</div>
+          <div class="row items-center q-gutter-sm">
+            <div v-for="(community, communityIndex) in communityList" :key="communityIndex">
+              <q-icon :name="community.icon" size="md" class="cursor-pointer" @click="routerTo(community.route)"></q-icon>
+            </div>
           </div>
+
+          <!-- 切换语言 -->
         </div>
-
-        <!-- 切换语言 -->
       </div>
-    </div>
 
-    <div class="q-mt-xl text-center">
-      <div class="text-grey">
-        {{ config.name }}
-        <q-icon name="copyright" size="xs"></q-icon>
-        <span>2023</span>
-        <span>Cookie settings</span>
+      <div class="q-mt-xl text-center">
+        <div class="text-grey">
+          {{ config.name }}
+          <q-icon name="copyright" size="xs"></q-icon>
+          <span>2023</span>
+          <span>Cookie settings</span>
+        </div>
       </div>
     </div>
   </div>
