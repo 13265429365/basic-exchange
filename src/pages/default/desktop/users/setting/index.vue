4ff71731772d5f084143d6e1e5879184a2111e75
饶兆锋 <245532242@qq.com>
2023-12-27 17:33:07 +0800

    更新用户信息

--- src/pages/default/desktop/users/setting/index.vue
+++ src/pages/default/desktop/users/setting/index.vue
@@ -1,201 +1,123 @@
 <template>
   <div style="padding: 48px 100px;">
     <div class="text-weight-bold text-h5">{{ $t('personalSetting') }}</div>
-    <!-- row -->
-    <div v-for="(item, i) in SettingList" :key="i" class="q-mt-lg">
-      <div class="row no-wrap justify-between">
-        <div>
-          <div class="row" style="min-width: 260px;">
-            <q-img class="q-mr-sm" width="24px" height="24px" :src="imageSrc('')"></q-img>
-            <div>
-              <div class="text-h6 text-color-3 text-weight-bold">{{ $t(item.arguments) }}</div>
-              <div style="font-size: 12px;">{{ item.decs }}</div>
+
+    <div class="q-mt-lg">
+      <div class="row justify-between items-center q-mb-lg" v-for="(setting, settingIndex) in settingsList" :key="settingIndex">
+        <div class="col-6">
+          <div class="text-body1">{{$t(setting.name)}}</div>
+          <div class="text-grey text-caption">{{$t(setting.desc)}}</div>
+        </div>
+        <div class="col">
+          <div v-if="setting.type == 'password'">
+            <div>******</div>
+          </div>
+          <div v-else-if="setting.type == 'avatar'">
+            <q-avatar>
+              <q-img no-spinner :src="imageSrc(setting.value)"></q-img>
+            </q-avatar>
+          </div>
+          <div v-else-if="setting.type == 'datePicker'">
+            {{ date.formatDate(setting.value * 1000, 'YYYY/MM/DD') }}
+          </div>
+          <div v-else class="text-body2">
+            <div v-if="setting.params == 'sex'">
+              <div v-if="setting.value == 1">{{$t('male')}}</div>
+              <div v-else-if="setting.value == 2">{{$t('female')}}</div>
+              <div v-else>{{$t('unknown')}}</div>
+            </div>
+            <div v-else>
+              {{ setting.value == '' ? '- -' : setting.value}}
             </div>
           </div>
         </div>
-        <div class="row items-center">
-          <q-avatar v-if="item.avatar" style="width: 34px;height: 34px;">
-            <q-img :src="imageSrc(item.content ? item.content : '')"></q-img>
-          </q-avatar>
-          <div v-if="item.title == 'Gender'" class="text-weight-medium">
-            {{ item.text }}
-          </div>
-          <div v-if="item.title == 'Birthday'" class="text-weight-medium">
-            {{ date.formatDate(item.content * 1000, 'YYYY-MM-DD') }}
-          </div>
-          <div v-if="item.title != 'Gender' && item.title != 'Birthday' && item.title != 'Avatar'"
-            class="text-weight-medium">{{
-              item.content }}
-          </div>
-
-        </div>
-        <div class="row items-end">
-          <q-btn @click="show(item)" class="bg-grey-3" no-caps :label="$t(item.btn)"></q-btn>
+        <div>
+          <q-btn flat :label="$t('edit')" class="bg-grey-1 text-grey-8" @click="openUpdateDialog(setting)"></q-btn>
         </div>
       </div>
-      <q-separator class="q-mt-md" style="background: #F1F1F1;" />
     </div>
+    <q-dialog v-model="dialogShow">
+      <q-card class="full-width">
+        <q-card-section>
+          <div class="text-h6">{{$t('edit')}}{{$t(currentSetting.name)}}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <div v-if="currentSetting.params == 'avatar'">
+
+          </div>
+
+          <div v-else-if="currentSetting.params == 'nickname'">
+            <q-input outlined v-model="params[currentSetting.params]"></q-input>
+          </div>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat :label="$t('cancel')" color="grey" v-close-popup />
+          <q-btn flat :label="$t('submit')" color="primary" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
-  <!--  -->
-  <update ref="edit" @UserInfo="UserInfo"></update>
+
+
 </template>
 
 <script lang="ts">
-import update from 'pages/default/desktop/users/setting/update.vue';
-
-import { defineComponent, reactive, toRefs, ref, onMounted, nextTick } from 'vue';
-import { imageSrc } from 'src/utils/index';
-import { getUserInfo } from 'src/apis/user';
-import { UserStore, UserInfoKey } from 'src/stores/user';
+import { defineComponent, reactive, toRefs } from 'vue';
+import {getUserInfo, updateInfo, updatePassword} from 'src/apis/user';
+import { imageSrc } from 'src/utils';
+import {UserStore} from 'stores/user';
 import { date } from 'quasar';
 
-
 export default defineComponent({
-  name: 'userIndex',
-  components: {
-    update,
-  },
-
+  name: 'SettingIndex',
   setup() {
     const $userStore = UserStore()
 
-    const edit = ref(null);
-
+    const userInfo = $userStore.userInfo
     const state = reactive({
-      //
-      SettingList: [
-        // arguments是请求的参数
-        // type==info基本资料,  type==secure密码（判断弹窗显示内容）
-        // headTitle弹窗标题
-        {
-          title: 'Nickname',
-          decs: 'Set a customized nickname for your profile.',
-          content: '',
-          btn: 'edit',
-          arguments: 'nickname',
-          type: 'info',
-          headTitle: 'Edit Nickname',
-        },
-        {
-          title: 'Avatar',
-          decs: 'Select an avatar to personalize your account.',
-          avatar: '1',
-          btn: 'edit',
-          arguments: 'avatar',
-          type: 'info',
-          headTitle: 'Edit Avatar',
-        },
-        {
-          title: 'Gender',
-          decs: 'Select your gender.',
-          content: '',
-          btn: 'edit',
-          arguments: 'sex',
-          type: 'info',
-          headTitle: 'Edit Gender',
-        },
-        {
-          title: 'Birthday',
-          decs: 'Select your birthday.',
-          content: '',
-          btn: 'edit',
-          arguments: 'birthday',
-          type: 'info',
-          headTitle: 'Edit Birthday',
-        },
-        {
-          title: 'Phone Number',
-          decs: 'Set a phonenumber for your profile.',
-          content: '',
-          btn: 'edit',
-          arguments: 'telephone',
-          type: 'info',
-          headTitle: 'Bind Phone Number',
-        },
-        {
-          title: 'E-mail',
-          decs: 'Set a E-mail for your profile.',
-          content: '',
-          btn: 'edit',
-          arguments: 'email',
-          type: 'info',
-          headTitle: 'Enter Email',
-        },
-        {
-          title: 'Password',
-          decs: 'Set a password for your profile.',
-          btn: 'edit',
-          arguments: 'password',
-          type: 'secure',
-          headTitle: 'Edit Password',
-        },
-        {
-          title: 'Secret Key',
-          decs: 'Set a Secret Key for your profile.',
-          btn: 'edit',
-          arguments: 'secretKey',
-          type: 'secure',
-          headTitle: 'Edit Secret Key',
-        },
-      ] as any,
+      dialogShow: false,
+      currentSetting: {} as any,
+      params: {} as any,
+      settingsList: [
+        {name: 'avatar', params: 'avatar', type: 'avatar', desc: 'avatarSmall', value: userInfo.avatar},
+        {name: 'nickname', params: 'nickname', type: 'text', desc: 'nicknameSmall', value: userInfo.nickname},
+        {name: 'email', params: 'email', type: 'text', desc: 'emailSmall', value: userInfo.email},
+        {name: 'password', params: 'password', type: 'password', desc: 'passwordSmall', value: ''},
+        {name: 'secretKey', params: 'secretKey', type: 'password', desc: 'secretKeySmall', value: ''},
+        {name: 'telephone', params: 'telephone', type: 'telephone', desc: 'telephoneSmall', value: userInfo.telephone},
+        {name: 'sex', params: 'sex', type: 'toggle', desc: 'sexSmall', value: userInfo.sex},
+        {name: 'birthday', params: 'birthday', type: 'datePicker', desc: 'birthdaySmall', value: userInfo.birthday},
+        {name: 'personalSignature', params: 'desc', type: 'textarea', desc: 'personalSignatureSmall', value: userInfo.desc},
+      ]
     });
 
-    onMounted(() => {
-      UserInfo()
-    })
-
-    const UserInfo = () => {
-      getUserInfo().then((res: any) => {
-        console.log('用户信息', res);
-        state.SettingList.forEach((Setting: any) => {
-          for (const resItem in res) {
-            if (Setting.arguments == resItem) {
-              Setting.content = res[resItem]
-            }
-          }
-        });
-        state.SettingList.forEach((Setting: any) => {
-          if (Setting.title == 'Gender') {
-            if (Setting.content == 0) {
-              Setting.text = '未填写'
-            }
-            if (Setting.content == 1) {
-              Setting.text = 'male'
-            }
-            if (Setting.content == 2) {
-              Setting.text = 'female'
-            }
-          }
-        })
-        $userStore.updateUserInfo(res)
-        localStorage.setItem(UserInfoKey, JSON.stringify(res))
-      })
+    // 打开弹窗
+    const openUpdateDialog = (setting: any) => {
+      state.dialogShow = true
+      state.currentSetting = setting
     }
 
+    // 更新用户信息
+    const submitFunc = () => {
 
-    const show = (row: any) => {
-      edit.value?.showEdit(row)
+    }
+
+    // 更新当前用户信息
+    const updateUserInfo = () => {
+      getUserInfo().then((res: any) => {
+        $userStore.updateUserInfo(res)
+      })
     }
 
     return {
       imageSrc,
       date,
       ...toRefs(state),
-      edit,
-      // 打开edit对话框
-      show,
-      UserInfo,
+      openUpdateDialog,
     }
   }
 });
 </script>
-<style lang="scss" scoped>
-.q-btn {
-  width: 80px;
-  height: 32px;
-}
-
-.q-btn:before {
-  box-shadow: none;
-}
-</style>
+<style lang="scss" scoped></style>
